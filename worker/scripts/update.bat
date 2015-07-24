435065d3d89789c18da38b7c0de9e8f680fde47b
Danylo Bilyk <daniil.bilyk@eglobal-forex.com>
2015-07-24 12:32:03 +0300

    Fixing update script.

--- worker/scripts/update.bat
+++ worker/scripts/update.bat
@@ -1,8 +1,8 @@
 SET PATH=%PATH%;c:\Program Files (x86)\Git\cmd
-git config core.autocrlf true
-git config user.email "daniil.bilyk@eglobal-forex.com"
-git config user.name "Danylo Bilyk"
-git update-index --assume-unchanged config.ini
-git stash
+git status | find "modified"
+set MODIFIED=%errorlevel%
+if %MODIFIED% EQ 0:
+    git stash
 git pull
-git stash pop
+if %MODIFIED% EQ 0:
+    git stash pop
