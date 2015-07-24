f64cc986f9882e900d1c09b47d546cab982d976b
Danylo Bilyk <daniil.bilyk@eglobal-forex.com>
2015-07-24 12:31:46 +0300

    Fixing update script.

--- setup.bat
+++ setup.bat
@@ -3,3 +3,9 @@ virtualenv .env
 call .env\Scripts\activate.bat
 pip install -e common
 pip show pt-common
+
+SET PATH=%PATH%;c:\Program Files (x86)\Git\cmd
+git config core.autocrlf true
+git config user.email "daniil.bilyk@eglobal-forex.com"
+git config user.name "Danylo Bilyk"
+git update-index --assume-unchanged config.ini
