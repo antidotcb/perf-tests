82fb6015877c62892510bba06f318ecdc6d6441a
Danylo Bilyk <daniil.bilyk@eglobal-forex.com>
2015-07-24 12:33:25 +0300

    Fixing update script.

--- worker/scripts/update.bat
+++ worker/scripts/update.bat
@@ -1,8 +1,8 @@
 SET PATH=%PATH%;c:\Program Files (x86)\Git\cmd
 git status | find "modified"
 set MODIFIED=%errorlevel%
-if %MODIFIED% EQ 0:
+if %MODIFIED% GTR 0
     git stash
 git pull
-if %MODIFIED% EQ 0:
+if %MODIFIED% GTR 0
     git stash pop
