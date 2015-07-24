a822c295648589f77f3ae9d66368e955dbc14732
Danylo Bilyk <daniil.bilyk@eglobal-forex.com>
2015-07-24 12:47:56 +0300

    Fixing update script.

--- setup.bat
+++ setup.bat
@@ -8,4 +8,4 @@ SET PATH=%PATH%;c:\Program Files (x86)\Git\cmd
 git config core.autocrlf true
 git config user.email "daniil.bilyk@eglobal-forex.com"
 git config user.name "Danylo Bilyk"
-git update-index --assume-unchanged config.ini
+git update-index --no-assume-unchanged config.ini
--- worker/scripts/update.bat
+++ worker/scripts/update.bat
@@ -1,8 +1,6 @@
 SET PATH=%PATH%;c:\Program Files (x86)\Git\cmd
 git status | find "modified"
 set MODIFIED=%errorlevel%
-if %MODIFIED% GTR 0
-    git stash
+if %MODIFIED% GTR 0 git stash
 git pull
-if %MODIFIED% GTR 0
-    git stash pop
+if %MODIFIED% GTR 0 git stash pop
